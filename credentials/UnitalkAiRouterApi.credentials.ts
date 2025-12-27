b1009c87cc0df03b79ff9a7f69e63b3156884c39
atouch mohamed <4213033+atouchsh@users.noreply.github.com>
2025-12-27 18:39:01 +0100

    init

---
+++ credentials/UnitalkAiRouterApi.credentials.ts
@@ -0,0 +1,90 @@
+import type {
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestOptions,
+	INodeProperties,
+	Icon,
+} from 'n8n-workflow';
+
+export class UnitalkAiRouterApi implements ICredentialType {
+	name = 'unitalkAiRouterApi';
+	displayName = 'Unitalk AI Router API';
+	icon: Icon = { light: 'file:../icons/unitalk.svg', dark: 'file:../icons/unitalk.dark.svg' };
+	documentationUrl = 'https://unitalk.ai';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			required: true,
+			default: '',
+		},
+		{
+			displayName: 'Base URL',
+			name: 'url',
+			type: 'string',
+			default: 'https://gateway.unitalk.ai/v1',
+			description: 'Override the default base URL for the API',
+		},
+		{
+			displayName: 'Add Custom Header',
+			name: 'header',
+			type: 'boolean',
+			default: false,
+		},
+		{
+			displayName: 'Header Name',
+			name: 'headerName',
+			type: 'string',
+			displayOptions: {
+				show: {
+					header: [true],
+				},
+			},
+			default: '',
+		},
+		{
+			displayName: 'Header Value',
+			name: 'headerValue',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			displayOptions: {
+				show: {
+					header: [true],
+				},
+			},
+			default: '',
+		},
+	];
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials?.url}}',
+			url: '/models',
+		},
+	};
+
+	async authenticate(
+		credentials: ICredentialDataDecryptedObject,
+		requestOptions: IHttpRequestOptions,
+	): Promise<IHttpRequestOptions> {
+		requestOptions.headers ??= {};
+
+		requestOptions.headers['Authorization'] = `Bearer ${credentials.apiKey}`;
+
+		if (
+			credentials.header &&
+			typeof credentials.headerName === 'string' &&
+			credentials.headerName &&
+			typeof credentials.headerValue === 'string'
+		) {
+			requestOptions.headers[credentials.headerName] = credentials.headerValue;
+		}
+
+		return requestOptions;
+	}
+}
---
+++ nodes/UnitalkAIRouter/UnitalkAiRouter.node.ts
@@ -0,0 +1,68 @@
+import type { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import { NodeConnectionTypes } from 'n8n-workflow';
+
+import { chatFields, chatOperations } from './ChatDescription';
+import { imageFields, imageOperations } from './ImageDescription';
+import { textFields, textOperations } from './TextDescription';
+
+export class UnitalkAiRouter implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Unitalk AI',
+		name: 'unitalkAiRouter',
+		hidden: true,
+		icon: { light: 'file:../../icons/unitalk.svg', dark: 'file:../../icons/unitalk.dark.svg' },
+		group: ['transform'],
+		version: [1, 1.1],
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Consume Open AI',
+		defaults: {
+			name: 'Unitalk AI Router',
+		},
+		inputs: [NodeConnectionTypes.Main],
+		outputs: [NodeConnectionTypes.Main],
+		credentials: [
+			{
+				name: 'unitalkAiRouterApi',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			ignoreHttpStatusErrors: true,
+			baseURL:
+				'={{ $credentials.url?.split("/").slice(0,-1).join("/") ?? "https://api.openai.com" }}',
+		},
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Chat',
+						value: 'chat',
+					},
+					{
+						name: 'Image',
+						value: 'image',
+					},
+					{
+						name: 'Text',
+						value: 'text',
+					},
+				],
+				default: 'text',
+			},
+
+			...chatOperations,
+			...chatFields,
+
+			...imageOperations,
+			...imageFields,
+
+			...textOperations,
+			...textFields,
+		],
+		usableAsTool: true,
+	};
+}
